bdabe8ea6a263d6283da21b6cd0d9746cd713e3e
Surya-at-555493241417 <surya.ganesan@verticurl.com>
2024-03-11 19:47:08 +0530

    test

---
+++ test.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement test2 final
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
