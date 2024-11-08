7bc6a28bff620ec2486f89b3c6b9921af04d77d6
gsurya736 <67796287+gsurya736@users.noreply.github.com>
2024-11-08 12:56:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement test2 final current
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda test with git hub !')
+        'body': json.dumps('Hello from Lambda test with git hub test!')
     }
